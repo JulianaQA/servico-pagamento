27ae295098c65b867a77fdced1ed7bcc4710d7f0
Juliana De Souza Pereira <julianadesouzapereira@MacBook-Air-de-Juliana.local>
2026-05-31 15:26:09 -0300

    docs: formata README e atualiza testar.js

--- testar.js
+++ testar.js
@@ -2,5 +2,4 @@ const ServicoDePagamento = require('./src/ServicoDePagamento');
 
 const servico = new ServicoDePagamento();
 servico.pagar('0987-7656-3475', 'Samar', 156.87);
-console.log('Resultado:');
 console.log(servico.consultarUltimoPagamento());
